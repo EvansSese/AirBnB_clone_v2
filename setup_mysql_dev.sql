94c90052dcd72a49b5b545985f4b53ba29e9df73
Biitopher <biitopher@gmail.com>
2023-09-20 13:51:43 -0700

    [Feature]Task 3

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,10 @@
+-- Script preparing MySQL server for the project
+-- Creates the database hbnb_dev_db
+-- Creates the user hbnb_dev
+-- Assigns the password hbnb_dev_pwd
+
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
