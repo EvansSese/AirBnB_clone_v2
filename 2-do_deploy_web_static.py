bd4d0cef5db9c7f8d61b811f2a5309af1d2f3978
Evans Sese <sese_evans@yahoo.com>
2023-10-05 08:57:00 -0700

    [Feature] Task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -12,29 +12,28 @@ def do_deploy(archive_path):
         return False
 
     try:
-        archive_name = os.path.basename(archive_path)
-        archive_no_ext = os.path.splitext(archive_name)[0]
-
-        # Upload the archive to /tmp/ directory on the server
+        file_name = archive_path.split("/")[-1].split(".")[0]
         put(archive_path, "/tmp/")
 
-        # Create the directory if it doesn't exist
-        run("mkdir -p /data/web_static/releases/{}/".format(archive_no_ext))
+        run("mkdir -p /data/web_static/releases/{}".format(file_name))
 
-        # Uncompress the archive
-        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(
-            archive_name, archive_no_ext))
+        run("tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/"
+            .format(file_name, file_name))
 
-        # Remove the uploaded archive
-        run("rm /tmp/{}".format(archive_name))
+        run('rm -rf /tmp/{}.tgz'.format(file_name))
 
-        # Delete the symbolic link /data/web_static/current
-        run("rm -rf /data/web_static/current")
+        run(('mv /data/web_static/releases/{}/web_static/* ' +
+            '/data/web_static/releases/{}/')
+            .format(file_name, file_name))
 
-        # Create a new symbolic link to the new version
-        run("ln -s /data/web_static/releases/{}/ /data/web_static/current".format(archive_no_ext))
+        run('rm -rf /data/web_static/releases/{}/web_static'
+            .format(file_name))
 
-        print("New version deployed!")
+        run('rm -rf /data/web_static/current')
+
+        run(('ln -s /data/web_static/releases/{}/' +
+            ' /data/web_static/current')
+            .format(file_name))
         return True
     except Exception:
         return False
