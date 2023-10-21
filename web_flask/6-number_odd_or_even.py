d9a2b9ef13c2bb542122d0505309aad050f2ac6e
Evans Sese <sese_evans@yahoo.com>
2023-10-21 10:50:26 +0300

    [Feature] Task 6

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+""" C Route"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ Function for home route"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Function for home route"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_with_text(text):
+    """ Function to get text from route"""
+    text = text.replace('_', ' ')
+    return "C " + text
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_with_text(text):
+    """ Function to get text from route, with default value"""
+    text = text.replace('_', ' ')
+    return "Python " + text
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def is_number(n):
+    """ Function to display is number """
+    return f"{n} is a number"
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    if isinstance(n, int):
+        return render_template('5-number.html', n=n)
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def number_odd_or_even(n):
+    """ Function to display if a number is odd or even"""
+    if isinstance(n, int):
+        odd_even = "odd" if n % 2 != 0 else "even"
+        return render_template('6-number_odd_or_even.html',
+                               n=n, odd_even=odd_even)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
