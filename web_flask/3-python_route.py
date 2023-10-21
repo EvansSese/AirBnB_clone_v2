8c0019ebe18b9e7c3837f5aaa33aa866ba0de089
Evans Sese <sese_evans@yahoo.com>
2023-10-21 09:47:40 +0300

    [Feature] Task 3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" C Route"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ Function for home route"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Function for home route"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_with_text(text):
+    """ Function to get text from route"""
+    text = text.replace('_', ' ')
+    return "C " + text
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_with_text(text):
+    """ Function to get text from route, with default value"""
+    text = text.replace('_', ' ')
+    return "Python " + text
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
