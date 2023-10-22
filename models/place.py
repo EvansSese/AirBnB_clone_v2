4ae345bbfe762c705ce1f567703b1d3b105ec7aa
Evans Sese <sese_evans@yahoo.com>
2023-10-22 12:12:06 +0300

    [Fix] Places class

--- models/place.py
+++ models/place.py
@@ -2,7 +2,6 @@
 """ Place Module for HBNB project """
 import models
 from models.base_model import BaseModel, Base
-from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
 from sqlalchemy.orm import relationship
