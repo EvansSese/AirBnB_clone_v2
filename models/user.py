1e681c03ab2a58f9fddd56a47883360e4df4fece
Evans Sese <sese_evans@yahoo.com>
2023-10-22 10:23:51 +0300

    [Fix] User class

--- models/user.py
+++ models/user.py
@@ -1,26 +1,29 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 
-
 import models
-from models.base_model import BaseModel, Base
+from models.base_model import BaseModel
 from sqlalchemy import Column, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
 
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    if models.storage_type == "db":
+    if models.storage_type == 'db':
         __tablename__ = 'users'
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
-    else:
+
+    def __init__(self, *args, **kwargs):
+        """Init user"""
+        super().__init__(*args, **kwargs)
         email = ""
         password = ""
         first_name = ""
         last_name = ""
 
-    def __init__(self, *args, **kwargs):
-        """Init user"""
-        super().__init__(*args, **kwargs)
+
